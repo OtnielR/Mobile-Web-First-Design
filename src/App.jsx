85ef61d4c100cc57163831ce626083c780fb73df
otnielr <endidebora@gmail.com>
2026-04-28 15:58:41 +0700

    Add game.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/home/home'
 import Settings from './pages/settings/settings'
 import ArticleDetails from './pages/article/article-details'
@@ -8,10 +8,31 @@ import Discover from './pages/discover/discover'
 
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true)
+    const handleOffline = () => {
+      console.log("MODE OFFLINE")
+      setIsOnline(false)
+
+      window.location.href = "game.html"
+
+    }
+
+    window.addEventListener("online", handleOnline)
+    window.addEventListener("offline", handleOffline)
+
+    if (!navigator.onLine) {
+      window.location.href = "game.html"
+    }
+
+
+  }, [])
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/settings" element={<Settings />}></Route>
@@ -19,7 +40,7 @@ function App() {
           <Route path="/bookmarked" element={<Bookmarked />}></Route>
           <Route path="/discover" element={<Discover />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
